d1629dee10b2093e573e5eec0ccd482fc16d83a8
Lucas de Macêdo Pinheiro <lucasmp10@hotmail.com>
2019-05-24 20:19:38 -0300

    Added code to append each of the 3 separated series

---
+++ JoinSeries.m
@@ -0,0 +1,88 @@
+% Load files
+% load('MATLAB_Matrix_Pilot_1M_Right_S1.mat')
+% load('MATLAB_Matrix_Pilot_1M_Right_S2.mat')
+% load('MATLAB_Matrix_Pilot_1M_Right_S3.mat')
+
+% Adjust the time to begin with zero
+Toffset = min([ForceTrim1.Time(1),StimCurrentTrim1.Time(1)]);
+ForceTrim1.Time = ForceTrim1.Time - Toffset;
+StimCurrentTrim1.Time = StimCurrentTrim1.Time - Toffset;
+StimCommandData1.Time = StimCommandData1.Time - Toffset;
+
+Toffset = min([ForceTrim2.Time(1),StimCurrentTrim2.Time(1)]);
+ForceTrim2.Time = ForceTrim2.Time - Toffset;
+StimCurrentTrim2.Time = StimCurrentTrim2.Time - Toffset;
+StimCommandData2.Time = StimCommandData2.Time - Toffset;
+
+Toffset = min([ForceTrim3.Time(1),StimCurrentTrim3.Time(1)]);
+ForceTrim3.Time = ForceTrim3.Time - Toffset;
+StimCurrentTrim3.Time = StimCurrentTrim3.Time - Toffset;
+StimCommandData3.Time = StimCommandData3.Time - Toffset;
+
+% Get rid of trending data with envelope function
+ForceTrim1bkp = ForceTrim1;
+ForceTrim2bkp = ForceTrim2;
+ForceTrim3bkp = ForceTrim3;
+
+[~,LPeakFit] = envelope(ForceTrim1.Data,80,'peak');
+ForceTrim1.Data = ForceTrim1.Data - LPeakFit;
+
+[~,LPeakFit] = envelope(ForceTrim2.Data,80,'peak');
+ForceTrim2.Data = ForceTrim2.Data - LPeakFit;
+
+[~,LPeakFit] = envelope(ForceTrim3.Data,80,'peak');
+ForceTrim3.Data = ForceTrim3.Data - LPeakFit;
+
+% Trim data at the end to concatenate 
+tf = StimCommandData1.Time(end);
+Force2NewTi = ForceTrim1.Time(find(ForceTrim1.Time>tf,1));
+ForceTrim1 = getsamples(ForceTrim1,1:find(ForceTrim1.Time==Force2NewTi)-1);
+StimCurr2NewTi = StimCurrentTrim1.Time(find(StimCurrentTrim1.Time>tf,1));
+StimCurrentTrim1 = getsamples(StimCurrentTrim1,1:find(StimCurrentTrim1.Time==StimCurr2NewTi)-1);
+StimCom2NewTi = StimCommandData1.Time(end);
+
+Force = ForceTrim2;
+Force.Time = ForceTrim2.Time + Force2NewTi;
+StimCurrent = StimCurrentTrim2;
+StimCurrent.Time = StimCurrentTrim2.Time + StimCurr2NewTi;
+StimCommand = StimCommandData2;
+StimCommand.Time = StimCommandData2.Time + StimCom2NewTi;
+
+Force = append(ForceTrim1, Force);
+StimCurrent = append(StimCurrentTrim1,StimCurrent);
+StimCommand = [StimCommandData1;StimCommand];
+
+StimCommandData1 = StimCommand;
+ForceTrim1 = Force;
+StimCurrentTrim1 = StimCurrent;
+ForceTrim2 = ForceTrim3;
+StimCurrentTrim2 = StimCurrentTrim3;
+StimCommandData2 = StimCommandData3;
+
+tf = StimCommandData1.Time(end);
+Force2NewTi = ForceTrim1.Time(find(ForceTrim1.Time>tf,1));
+ForceTrim1 = getsamples(ForceTrim1,1:find(ForceTrim1.Time==Force2NewTi)-1);
+StimCurr2NewTi = StimCurrentTrim1.Time(find(StimCurrentTrim1.Time>tf,1));
+StimCurrentTrim1 = getsamples(StimCurrentTrim1,1:find(StimCurrentTrim1.Time==StimCurr2NewTi)-1);
+StimCom2NewTi = StimCommandData1.Time(end);
+
+Force = ForceTrim2;
+Force.Time = ForceTrim2.Time + Force2NewTi;
+StimCurrent = StimCurrentTrim2;
+StimCurrent.Time = StimCurrentTrim2.Time + StimCurr2NewTi;
+StimCommand = StimCommandData2;
+StimCommand.Time = StimCommandData2.Time + StimCom2NewTi;
+
+Force = append(ForceTrim1, Force);
+StimCurrent = append(StimCurrentTrim1,StimCurrent);
+StimCommand = [StimCommandData1;StimCommand];
+
+% Plots final data
+figure
+plot(Force)
+hold on
+plot(StimCurrent./40)
+plot(StimCommand.Time,1.5*ones(1,length(StimCommand.Time)),'.')
+
+% Saves data
+save('')
--- PlotFromMatData.m
+++ PlotFromMatData.m
@@ -1,13 +1,44 @@
 %
-% MATLAB code - EMA Matrix Experiments
-% 2019-05-16
-% Lucas de Macedo Pinheiro
 %
+% MATLAB code - EMA Matrix Experiments
+% 2019-05-19
+% Lucas de Macedo Pinheiro
+% 
+%   Plot the stimulator, current and force data 
+% from mat files.
+%
+%
+Files = dir('MATLAB*Right*.mat');
 
+for w = 1:length(Files)
+    
+    load(Files(w).name);
+    
     figure;
     plot(ForceTrim);
     hold on
     plot(StimCurrentTrim./40);
-    title(Files(w).name)
+    title(Files(w).name(8:end-4))
     ylim([-0.5 2.5])
     hold off
+end
+
+Files = dir('MATLAB*Left*.mat');
+
+for w = 1:length(Files)
+    
+    load(Files(w).name);
+    
+    figure;
+    plot(ForceTrim);
+    
+    hold on
+    plot(StimCurrentTrim./40);
+    title(Files(w).name(8:end-4))
+    ylim([-0.5 2.5])
+    
+    %shade = area([StimCommandData.Time(100),StimCommandData.Time(400)],[10,10],2);
+    %shade.FaceColor = 'k'; shade.FaceAlpha = 0.3; shade.EdgeAlpha = 0.3;
+    
+    hold off
+end
