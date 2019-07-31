907cbdf4ead0af31294714787294c053d804dd3d
Lucas de Macêdo Pinheiro <lucasmp10@hotmail.com>
2019-07-31 15:48:35 -0300

    Added the final Matlab code. GatherAllStats doesnt always work.

--- ForceAreaFitting.m
+++ ForceAreaFitting.m
@@ -16,10 +16,19 @@ if isa(Files,'char') % Only one file selected
    Files = {Files}; 
 end
 
+ExcludeIdx = [];
+% ExcludeIdx = [1,2];
+
 for w = 1:length(Files)
 % Import MAT file
     fprintf('\n\nLoading "%s" file...\n', Files{w});
     load(Files{w});
+    %%
+% Normalize the force data
+    disp('Normalizing force...');
+    ForceNorm = Force;
+    ForceNorm.Data = ForceNorm.Data/max(ForceNorm.Data);
+%     ForceNorm.Data = ForceNorm.Data/max(ForceNorm.Data(229:end));
     
 % Calculate the force integral during stimulation sequences
     disp('Calculating areas...');
@@ -27,7 +36,7 @@ for w = 1:length(Files)
     StimEdges = sort([StimEdges; StimEdges+1]); % Both rising and falling edges
     StimEdges = StimCommandZeroed.Time([1; StimEdges; end]); % Get time
 
-    CompareM = pdist2(Force.Time,StimEdges); % Compares each element
+    CompareM = pdist2(ForceNorm.Time,StimEdges); % Compares each element
     [~,AllIndicesVec] = min(CompareM); % Get index of closest time
     IndexM = vec2mat(AllIndicesVec,2); % Column1 is start, column2 is end
 
@@ -35,36 +44,55 @@ for w = 1:length(Files)
     AreasVec = zeros(length(IndexM),1);
     for i=1:length(IndexM)
         IdxVec = IndexM(i,1):IndexM(i,2);
-        ForceStimCell{i,1} = [Force.Time(IdxVec) Force.Data(IdxVec)];
+        ForceStimCell{i,1} = [ForceNorm.Time(IdxVec) ForceNorm.Data(IdxVec)];
         AreasVec(i,1) = trapz(ForceStimCell{i}(:,1),ForceStimCell{i,1}(:,2));
     end
+%     MidStimTimes = ForceNorm.Time(round(mean(IndexM,2)));
+    MidStimTimes = (7.5:10:357.5)';
 
 % Perform data fitting w/ poly1 (a*x + b)
+%
+% Default:
+%	Normalize:'off', Exclude:[], Weights:[], Method:'NonlinearLeastSquares',
+%   Robust:'Off', StartPoint:[1?0 double], Lower:[1?0 double],
+%   Upper:[1?0 double], Algorithm:'Trust-Region', DiffMinChange:1.0000e-08,
+%   DiffMaxChange:0.1000, Display:'Notify', MaxFunEvals:600, MaxIter:400,
+%   TolFun:1.0000e-06, TolX: 1.0000e-06
+
     disp('Fitting line...');
-    MidStimTimes = Force.Time(round(mean(IndexM,2)));
-    [CurveFitData,~,~] = fit(MidStimTimes,AreasVec,'poly1');
+    [CurveFitData,CurveFitStats,~] = fit(MidStimTimes,AreasVec,'poly1',...
+        'Exclude', ExcludeIdx);
+    AreasVecValid = AreasVec;
+    AreasVecValid(ExcludeIdx) = [];
     CurveFitCoefs = coeffvalues(CurveFitData); % Get a and b
-    CurveFitEq = sprintf('%.4ft %+ .4f',CurveFitCoefs);
-    disp(CurveFitData);
+    CurveFitCI = confint(CurveFitData); % Get a and b 95% intervals
+    CurveFitEq = sprintf('%.3et %+ .3f',CurveFitCoefs);
+    fprintf('COEF: %.3f(%.3f,%.3f)\n',1e3*CurveFitCoefs(1),1e3*CurveFitCI(1,1),1e3*CurveFitCI(2,1));
+    FTI.mean = mean(AreasVecValid);
+    FTI.std = std(AreasVecValid);
+    fprintf('FTI (Mean?SD): %.3f?%.3f\n',FTI.mean,FTI.std);
 
     figure
     hold on
     yyaxis left % Left Y axis 
-    plot(Force)
+    plot(ForceNorm)
     ylabel('Force (kgf)'), xlim([-10 380])
-    title(Files{w}(15:end-4),'Interpreter', 'none')
-    LimitsL = ylim;
+    title(Files{w}(1:end-4),'Interpreter', 'none')
+    LimitsL = [-.19 2]; ylim(LimitsL); box on;
     yyaxis right  % Right Y axis 
-    plot(MidStimTimes,AreasVec,'o')
-    plot(CurveFitData)
-    LimitsR = ylim;
+    plot(CurveFitData,MidStimTimes,AreasVec,'o',ExcludeIdx,'x')
+    LimitsR = [0 6.2];
     ylim([LimitsL(1)*LimitsR(2)/LimitsL(2) LimitsR(2)])
-    ylabel('Area (kgf.s)'), xlabel('Time (s)')
-    legend('Force','Area',CurveFitEq)
+    ylabel('Force-Time Integral (kgf.s)'), xlabel('Time (s)')
+    legend('Force','FTI','FTI Fit','Location','Best')
+%     legend('Force','FTI','Excluded','FTI Fit','Location','Best')
     hold off
     
     disp('Saving figure and file...');
-    savefig([Files{w}(15:end-4) '_Fitted.fig']);
+    savefig([Files{w}(1:end-4) '_Fitted.fig']);
+    save(Files{w},'ForceNorm','ExcludeIdx','MidStimTimes','AreasVec',...
+        'CurveFitData','CurveFitStats','AreasVecValid','CurveFitCoefs',...
+        'CurveFitCI','CurveFitEq','FTI','-append'); % '-v7'
 end
 
 
---
+++ GatherAllStats.m
@@ -0,0 +1,85 @@
+%
+% MATLAB code - EMA Matrix Experiments
+% 2019-07-18
+% Lucas de Macedo Pinheiro
+% 
+%   Gather data from mat files and store it.
+%
+%
+
+% clear; close all;
+
+% Open window for file selection
+disp('Select the MAT files...');
+Files = uigetfile('*.mat','Select The MAT Files','MultiSelect','on');
+if isa(Files,'char') % Only one file selected
+   Files = {Files}; 
+end
+%%
+for w = 1:length(Files)
+% Import MAT file
+    fprintf('\n\nLoading "%s" file...\n', Files{w});
+    TempStruct.(['F' num2str(w)]) = load(Files{w});
+end
+%%
+for w = 1:length(Files)
+    fprintf('\n\n"%s" file...\n', Files{w});
+
+    CurveFitCoefs = TempStruct.(['F' num2str(w)]).CurveFitCoefs;
+    CurveFitCI = TempStruct.(['F' num2str(w)]).CurveFitCI;
+    FTI = TempStruct.(['F' num2str(w)]).FTI;
+    R2 = TempStruct.(['F' num2str(w)]).CurveFitStats.rsquare;
+    RMSE = TempStruct.(['F' num2str(w)]).CurveFitStats.rmse;
+    
+    fprintf('COEF: %.3f(%.3f,%.3f)\n',1e3*CurveFitCoefs(1),1e3*CurveFitCI(1,1),1e3*CurveFitCI(2,1));
+    fprintf('FTI (Mean?SD): %.3f?%.3f\n',FTI.mean,FTI.std);
+    fprintf('R2: %.3f   RMSE: %.3f\n',R2,RMSE);
+end
+
+
+
+
+
+
+
+
+% clear; close all;
+% 
+% % Open window for file selection
+% disp('Select the MAT files...');
+% Files = uigetfile('*.mat','Select The MAT Files','MultiSelect','on');
+% if isa(Files,'char') % Only one file selected
+%    Files = {Files}; 
+% end
+% 
+% for w = length(Files):-1:1
+% % Import MAT file
+%     fprintf('\n\nLoading "%s" file...\n', Files{w});
+%     TempStruct.(['F' num2str(w)]) = load(Files{w});
+% 
+% % Construct a table with all the data
+%     FileTable = struct2table(TempStruct.(['F' num2str(w)]),'AsArray',true);
+%     FileName = {Files{w}(1:end-4)};
+%     FileTable.ExcludeIdx = {FileTable.ExcludeIdx};
+%     TempTable = [table(FileName), FileTable];
+%     StatsTable(w,:) = TempTable; 
+%     fprintf('COEF: %.3f(%.3f,%.3f)\n',1e3*StatsTable.CurveFitCoefs(w,1),1e3*StatsTable.CurveFitCI{w}(1,1),1e3*StatsTable.CurveFitCI{w}(2,1))
+%     fprintf('FTI (Mean?SD): %.3f?%.3f\n',StatsTable.FTI(w).mean,StatsTable.FTI(w).std);
+% end
+
+
+
+
+
+
+
+% Parse the big table
+% find(any(ismember(char(StatsTable.FileName{:,1}),'o0'),2))
+% AllSES = StatsTable([2,3,5],:);
+
+% Print for the tables
+% fprintf('\nCOEF: %.3f(%.3f,%.3f)\n',1e3*AllSES.CurveFitCoefs(r,1),1e3*AllSES.CurveFitCI{r}(1,1),1e3*AllSES.CurveFitCI{r}(2,1))
+
+
+
+
